386396aac262662e07b5d7cf9b51303aa4fc3857
spiegan77 <stephenpiegan@outlook.com>
2022-04-26 19:28:48 -0400

    Rotate Ship

--- ArgonAssault/Assets/Scripts/PlayerControls.cs
+++ ArgonAssault/Assets/Scripts/PlayerControls.cs
@@ -8,16 +8,45 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 5f;
 
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 2f; // Rick's Code
+    [SerializeField] float controlRollFactor = -20f; // Rick's Code
+    //[SerializeField] float controlYawFactor = 20f; // My code
+    //[SerializeField] float controlrollFactor = -20f; // My code
+
+    float xThrow, yThrow;
+
     void Update()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor; // Rick's Code
+        float roll = xThrow * controlRollFactor; // Rick's Code
+        //float yaw = transform.localPosition.x + xThrow * controlYawFactor; // My code
+        //float roll = transform.localPosition.x + xThrow * controlrollFactor; // My code
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         //Movement on x axis
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-        
+
         //Movement on y axis
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yOffset;
