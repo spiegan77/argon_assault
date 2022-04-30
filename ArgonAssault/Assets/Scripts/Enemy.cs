ee2261d48ba917af372376206b23e0d15ac7f291
spiegan77 <stephenpiegan@outlook.com>
2022-04-30 15:02:51 -0400

    Scoreboard

--- ArgonAssault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Assets/Scripts/Enemy.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
