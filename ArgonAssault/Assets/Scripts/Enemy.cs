9dbf45df67b52f6de8d9bc6df66d45834b0efe31
spiegan77 <stephenpiegan@outlook.com>
2022-05-01 15:53:57 -0400

    Enemy_Wave_Timeline

--- ArgonAssault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Assets/Scripts/Enemy.cs
@@ -5,30 +5,55 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
 
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] int hitPoints = 10;
+    // [SerializeField] int damage = 1;
+
+    
+
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidbody();
     }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        //enemyHitPoints -= DAMAGE
+        hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
+        //if enemyHitPoints <= 0
+          //Killenemy();
     }
 
     void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 }
